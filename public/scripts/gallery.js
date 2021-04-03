eb6a8fd2e1a6c707194f014d2d3d6096f0d89311
Rohan Bhushan <rbhushans@gmail.com>
2021-04-03 15:34:40 -0700

    fixed weird image thing

--- public/scripts/gallery.js
+++ public/scripts/gallery.js
@@ -1,19 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(){
     const displayedImage = document.querySelector('.displayed-img');
     const thumbBar = document.querySelector('.thumbnails');
-    let w = window.innerWidth;
     let gallery = ["hoops.jpg", "cherry.jpg", "houses.jpg", "corner.jpg"];
     for(let i = 0; i < gallery.length; i++) {
-        console.log("working")
         const newImage = document.createElement('img');
-        //add code checking screen size to select appropriate size
         newImage.setAttribute('src', `assets/${gallery[i]}`);
         newImage.setAttribute('alt', 'Thumbnail ' + i )
         thumbBar.appendChild(newImage);
         newImage.onclick = function(e) {
-            let width = window.innerWidth;
-            if (width < 7000) displayedImage.src = e.target.src;
-            else document.getElementById("big-displayed-img").srcset = e.target.src
+            displayedImage.src = e.target.src;
         }
     }
 })
